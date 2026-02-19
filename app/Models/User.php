30e72b5227eb8d52e4af81b92664dee9218b58f1
eazyyz <eazyyyyyyz@gmail.com>
2026-02-19 20:13:17 +0500

    Установка Sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,28 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TextControllerAPI;
 use App\Http\Controllers\UserControllerAPI;
 use App\Http\Controllers\TokenControllerAPI;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route:: get('/texts', [TextControllerAPI::class, 'index']);
-Route::get('/texts/{id}', [TextControllerAPI::class, 'show']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//Route:: get('/texts', [TextControllerAPI::class, 'index']);
+//Route::get('/texts/{id}', [TextControllerAPI::class, 'show']);
 
 Route:: get('/users', [UserControllerAPI::class, 'index']);
 Route::get('/users/{id}', [UserControllerAPI::class, 'show']);
 
 Route:: get('/tokens', [TokenControllerAPI::class, 'index']);
 Route::get('/tokens/{id}', [TokenControllerAPI::class, 'show']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::middleware('auth:sanctum')->get('/texts', [TextControllerAPI::class, 'index']);
+    Route::middleware('auth:sanctum')->get('/texts/{id}', [TextControllerAPI::class, 'show']);
+    Route::middleware('auth:sanctum')->get('/users', function (Request $request) {
+        return $request->user();
+    });
+    Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+});
